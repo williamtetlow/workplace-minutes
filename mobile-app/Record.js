6fac9860c298921542addad19b068f4226f5a40e
James Maciver <j.maciver22@gmail.com>
2017-07-18 14:30:51 +0100

    can record files to a temporary location

--- mobile-app/Record.js
+++ mobile-app/Record.js
@@ -1,15 +1,23 @@
-import React, { Component } from 'react';
+import React, { 
+	Component 
+} from 'react';
 import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Platform,
+  PermissionsAndroid,
 } from 'react-native';
 import { 
   FormLabel, 
   FormInput, 
   Button 
 } from 'react-native-elements'
+import {
+	AudioRecorder, 
+	AudioUtils
+} from 'react-native-audio';
 
 export default class Record extends Component {
 	state = {
@@ -17,11 +25,105 @@ export default class Record extends Component {
     recording: false,
     stoppedRecording: false,
     finished: false,
-    hasPermission: undefined,
+		audioPath: AudioUtils.DocumentDirectoryPath + '/test.aac',
+    hasPermission: undefined
 	};
 
+	_checkPermission() {
+		if (Platform.OS !== 'android') {
+			return Promise.resolve(true);
+		}
+
+		const rationale = {
+			'title': 'Microphone Permission',
+			'message': 'WorkplaceMobile needs access to your microphone so you can record audio.'
+		};
+
+		return PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.RECORD_AUDIO, rationale)
+			.then((result) => {
+				console.log('Permission result:', result);
+				return (result === true || result === PermissionsAndroid.RESULTS.GRANTED);
+			});
+	}
+
+	_prepareRecordingPath(audioPath){
+		AudioRecorder.prepareRecordingAtPath(audioPath, {
+			SampleRate: 22050,
+			Channels: 1,
+			AudioQuality: "Low",
+			AudioEncoding: "aac",
+			AudioEncodingBitRate: 32000
+		});
+	}
+
+	_finishRecording(didSucceed, filePath) {
+		this.setState({ finished: didSucceed });
+		console.log(`Finished recording of duration ${this.state.timeRecorded} seconds at path: ${filePath}`);
+	}
+
+	async _stopRecording() {
+		if (!this.state.recording) {
+			console.warn('Can\'t stop, not recording!');
+			return;
+		}
+
+		this.setState({stoppedRecording: true, recording: false});
+
+		try {
+			const filePath = await AudioRecorder.stopRecording();
+
+			if (Platform.OS === 'android') {
+				this._finishRecording(true, filePath);
+			}
+			return filePath;
+		} catch (error) {
+			console.error(error);
+		}
+	}
+
+	async _record() {
+		if (this.state.recording) {
+			console.warn('Already recording!');
+			return;
+		}
+
+		if (!this.state.hasPermission) {
+			console.warn('Can\'t record, no permission granted!');
+			return;
+		}
+
+		if(this.state.stoppedRecording){
+			this._prepareRecordingPath(this.state.audioPath);
+		}
+
+		this.setState({recording: true});
+
+		try {
+			const filePath = await AudioRecorder.startRecording();
+		} catch (error) {
+			console.error(error);
+		}
+	}
+
 	componentDidMount() {
-		//set up audio recorder here
+		this._checkPermission().then((hasPermission) => {
+			this.setState({ hasPermission });
+
+			if (!hasPermission) return;
+
+			this._prepareRecordingPath(this.state.audioPath);
+
+			AudioRecorder.onProgress = (data) => {
+				this.setState({timeRecorded: Math.floor(data.currentTime)});
+			};
+
+			AudioRecorder.onFinished = (data) => {
+				// Android callback comes in the form of a promise instead.
+				if (Platform.OS === 'ios') {
+					this._finishRecording(data.status === "OK", data.audioFileURL);
+				}
+			};
+		});
 	}
 
   render() {
@@ -34,7 +136,8 @@ export default class Record extends Component {
 				<Button
 					buttonStyle={styles.recordButton}
 					large
-					title='Start Recording' />
+					title={!this.state.recording ? 'Start Recording' : 'Complete Recording'}
+					onPress={() => !this.state.recording ? this._record() : this._stopRecording()} />
 			</View>
     );
   }
--- mobile-app/index.android.js
+++ mobile-app/index.android.js
@@ -2,9 +2,6 @@ import React, { Component } from 'react';
 import {
   AppRegistry
 } from 'react-native';
-import {
-  StackNavigator,
-} from 'react-navigation';
 import Login from './Login';
 import Record from './Record';
 
